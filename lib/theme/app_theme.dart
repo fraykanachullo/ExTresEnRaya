b38d99a798b22673f2d59186e05d5db67aaa7930
fraykana <fkanachullo12@gmail.com>
2024-05-03 14:03:36 -0500

    examentres en raya

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:flutter/widgets.dart';
+
+class AppTheme{
+
+static const Color blue =  Color(0xffff0026);
+static const Color red =  Color.fromARGB(255, 218, 43, 43);
+static const Color yellow = 
+// #fff04d
+Color(0xffa9ff4d);
+
+
+}
